3a40e6a71dd77992392e7bb3418b52156570b402
Hu Wei <hwhuige@gmail.com>
2025-05-15 11:06:13 +0800

    add log

--- llava/train/train.py
+++ llava/train/train.py
@@ -791,6 +791,9 @@ def train(attn_implementation=None):
     parser = transformers.HfArgumentParser(
         (ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    print(f"[train.train] model_args: {model_args}")
+    print(f"[train.train] data_args: {data_args}")
+    print(f"[train.train] training_args: {training_args}")
     local_rank = training_args.local_rank
     compute_dtype = (torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32))
 
